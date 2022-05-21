07af6111a6a82fe7f5d4fc59031941f08472db53
bolucloud <bolu@bolu.cloud>
2022-05-21 16:27:07 -0500

    add dynamodb + package

---
+++ ddb.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "customers_table" {
+  name           = "customer-list-api"
+  hash_key       = "customerId"
+  billing_mode   = "PAY_PER_REQUEST"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "customerId"
+    type = "S"
+  }
+
+  tags = {
+    gh-project = "customers_api"
+
+  }
+}
