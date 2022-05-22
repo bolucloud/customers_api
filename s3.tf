215b23acce968dff5823777f595009eaaa2f09b4
bolucloud <bolu@bolu.cloud>
2022-05-22 08:06:00 -0500

    update

--- s3.tf
+++ s3.tf
@@ -9,6 +9,7 @@ resource "aws_s3_bucket" "customer_api_bucket" {
 resource "aws_s3_bucket_object" "customer_api_ebs_deployment" {
   bucket = aws_s3_bucket.customer_api_bucket.id
   key    = "customer-api-key"
+  acl    = "public-read"
   source = "./Archive.zip"
   tags = {
     gh-project = "customers_api"
